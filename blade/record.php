d7b0d25d16be30373f72fd96a6e27ad11f471e85
binga288 <binga288@gmail.com>
2021-12-20 11:18:50 +0800

    紀錄完成

--- blade/record.php
+++ blade/record.php
@@ -1,3 +1,28 @@
 <?php
-    $arr = $db->select("rent_record");
+$array = $db->select("rent_record");
 ?>
+
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <td>#</td>
+                <td>學號</td>
+                <td>借閱日期</td>
+                <td>須歸還日期</td>
+                <td><a href="&test=123">是否歸還 ↓↑</a></td>
+                <td>書名</td>
+            </tr>
+        </thead>
+        <?php foreach ($array as $arr) { ?>
+            <tr>
+                <td><?= $arr["id"] ?></td>
+                <td><?= $arr["student_id"] ?></td>
+                <td><?= $arr["rent_date"] ?></td>
+                <td><?= $arr["return_date_limit"] ?></td>
+                <td><?= $arr["type"] == 0 ? "尚未歸還" : "歸還" ?></td>
+                <td><?= $arr["name"] ?></td>
+            </tr>
+        <?php } ?>
+    </table>
+</div>
