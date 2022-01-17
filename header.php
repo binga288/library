ad4c232d5e59f0dc57570ad86c6bdbd2f48cde9d
binga288 <binga288@gmail.com>
2022-01-17 09:19:13 +0800

    [BUG]Sidebar文字、SQL查詢錯誤

--- header.php
+++ header.php
@@ -120,15 +120,15 @@ $link = "http://$_SERVER[HTTP_HOST]/";
                                 <svg class="bi me-2" width="16" height="16">
                                     <use xlink:href="#people-circle" />
                                 </svg>
-                                還書
+                                借書
                             </a>
                         </li>
                         <li>
                             <a href="<?= $link ?>admin/return.php" class="nav-link text-white">
                                 <svg class="bi me-2" width="16" height="16">
                                     <use xlink:href="#people-circle" />
-                                </svg>
-                                借書
+                                </svg>                                
+                                還書
                             </a>
                         </li>
                         <li class="text-center mt-4">
